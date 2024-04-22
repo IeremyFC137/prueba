935d65ff37dbc7ae54f23e062d1107cc0c25f5e9
IeremyFC137 <ifloresc@cesel.com.pe>
2024-04-22 15:20:38 -0500

    dataset final

--- QueryListado.sql
+++ QueryListado.sql
@@ -1,3 +1,5 @@
+USE CERTCONT_GI
+GO
 select 
    case when [cc_tipo] = '0' then 'CERTIFICADOS'
 	  when [cc_tipo] = '1' then 'CONTRATOS'
@@ -9,138 +11,120 @@ select
 		[cc_ano] AñoProyecto
       ,[cc_area] AreaProyecto
       ,[cc_costo] CCostoProyecto
-	  ,[ct_desc] DescContractual
+	 -- ,replace([ct_desc], char(13)+char(10),' ') DescContractual
+	  ,replace((SELECT ct_desc_larga from ERP_TEST..PUB_TA_CCOSTOS where cc_cia = '01' and cc_costo = PUB_TA_CERTIFICADOS.cc_costo), char(13)+char(10),' ') DescContractual
 	  ,[ct_moneda_orig] MonedaOrigen
       ,[ni_importe_orig] ImporteMonedaOrigen
 	  ,[ni_importe_mc] ImporteMonedaComercialUS$
 	  ,[ni_tcambio] TCambio
 	  ,[df_inicio] FInicio
       ,[df_termino] FTermino
-      ,[ct_ruta] RutaDocumento    
-      
-      ,[cc_ruc]
-      ,[ct_raz_social_cliente]
-      
-     -- ,[ct_cliente_consorcio]
-     -- ,[cc_lib_tributaria]
+      ,replace([ct_ruta], char(13)+char(10),' ') RutaDocumento          
+      ,[cc_ruc] RucCliente
+      ,replace([ct_raz_social_cliente], char(13)+char(10),' ') RazonSocialCliente
       --,[cfl_servicio]
       , case when [cfl_servicio] = '1' then 'ESTUDIOS Y DISEÑOS'
 	  when [cfl_orig] = '2' then 'ESTUDIOS Y DISEÑOS'
 	  when [cfl_orig] = '3' then 'GERENCIAMIENTO DE PROYECTOS'
-	  else 'SIN TIPO DE SERVICIO'
+	  else 'OTROS SERVICIOS'
 	  end TipoServicio
-	  ,[cfl_orig]
-      --,[ct_desc_adicional] 
+	  ,[cfl_orig]    
 
 from PUB_TA_CERTIFICADOS  
---WHERE cc_area = 'TP' 
+WHERE coalesce(cc_area,'') <> '' and cc_area <> 'NN'
 UNION ALL 
-select 
-   case when [cc_tipo] = '0' then 'CERTIFICADOS'
-	  when [cc_tipo] = '1' then 'CONTRATOS'
-	  when [cc_tipo] = '2' then 'ANEXOS'
-	  when [cc_tipo] = '4' then 'ADENDAS'
-	  else 'SIN DOCUMENTO'
-	  end TipoDoc,
-
-	 [cc_ano]
-      ,[cc_area]
-      ,[cc_costo]
-	  ,[ct_desc]
-	  ,[ct_moneda_orig]
-      ,[ni_importe_orig]
-	  ,[ni_importe_mc]
-	  ,[ni_tcambio]
-	  ,[df_inicio]
-      ,[df_termino]
-      ,[ct_ruta]
-      
-      
+	 select
+	 case when [cc_tipo] = '0' then 'CERTIFICADOS'
+		  when [cc_tipo] = '1' then 'CONTRATOS'
+		  when [cc_tipo] = '2' then 'ANEXOS'
+		  when [cc_tipo] = '4' then 'ADENDAS'
+		  else 'SIN DOCUMENTO'
+		  end TipoDoc,
+	   [cc_ano] AñoProyecto
+      ,[cc_area] AreaProyecto
+      ,[cc_costo] CCostoProyecto
+	  --,replace([ct_desc], char(13)+char(10),' ') DescContractual
+	  ,replace((SELECT ct_desc_larga from ERP_TEST..PUB_TA_CCOSTOS where cc_cia = '01' and cc_costo = PUB_TA_CONTRATOS.cc_costo), char(13)+char(10),' ') DescContractual
+	  ,[ct_moneda_orig] MonedaOrigen
+      ,[ni_importe_orig] ImporteMonedaOrigen
+	  ,[ni_importe_mc] ImporteMonedaComercialUS$
+	  ,[ni_tcambio] TCambio
+	  ,[df_inicio] FInicio
+      ,[df_termino] FTermino
+      ,replace([ct_ruta], char(13)+char(10),' ') RutaDocumento          
       ,[cc_ruc]
-      ,[ct_raz_social_cliente]      
-      --,[ct_cliente_consorcio]
-      --,[cc_lib_tributaria]
-     --,[cfl_servicio]
+      ,replace([ct_raz_social_cliente], char(13)+char(10),' ') 
+      --,[cfl_servicio]
       , case when [cfl_servicio] = '1' then 'ESTUDIOS Y DISEÑOS'
 	  when [cfl_orig] = '2' then 'ESTUDIOS Y DISEÑOS'
 	  when [cfl_orig] = '3' then 'GERENCIAMIENTO DE PROYECTOS'
-	  else 'SIN TIPO DE SERVICIO'
+	  else 'OTROS SERVICIOS'
 	  end TipoServicio
-	  ,[cfl_orig]
-     -- ,[ct_desc_adicional] 
+	  ,[cfl_orig]   
 	   from dbo.PUB_TA_CONTRATOS  
-	   --WHERE cc_area = 'TP'	  
-	  UNION ALL 
-	  select  
-	     case when [cc_tipo] = '0' then 'CERTIFICADOS'
+	   WHERE coalesce(cc_area,'') <> '' and cc_area <> 'NN'  
+ UNION ALL 
+	  select
+	  case when [cc_tipo] = '0' then 'CERTIFICADOS'
 	  when [cc_tipo] = '1' then 'CONTRATOS'
 	  when [cc_tipo] = '2' then 'ANEXOS'
 	  when [cc_tipo] = '4' then 'ADENDAS'
 	  else 'SIN DOCUMENTO'
 	  end TipoDoc,
-	  [cc_ano]
-      ,[cc_area]
-      ,[cc_costo]
-	  ,[ct_desc]
-	  ,[ct_moneda_orig]
-      ,[ni_importe_orig]
-	  ,[ni_importe_mc]
-	  ,[ni_tcambio]
-	  ,[df_inicio]
-      ,[df_termino]
-      ,[ct_ruta]
-            
+	   [cc_ano] AñoProyecto
+      ,[cc_area] AreaProyecto
+      ,[cc_costo] CCostoProyecto
+	  --,replace([ct_desc], char(13)+char(10),' ') DescContractual
+	  ,replace((SELECT ct_desc_larga from ERP_TEST..PUB_TA_CCOSTOS where cc_cia = '01' and cc_costo = PUB_TA_ANEXOS.cc_costo), char(13)+char(10),' ') DescContractual
+	  ,[ct_moneda_orig] MonedaOrigen
+      ,[ni_importe_orig] ImporteMonedaOrigen
+	  ,[ni_importe_mc] ImporteMonedaComercialUS$
+	  ,[ni_tcambio] TCambio
+	  ,[df_inicio] FInicio
+      ,[df_termino] FTermino
+      ,replace([ct_ruta], char(13)+char(10),' ') RutaDocumento          
       ,[cc_ruc]
-      ,[ct_raz_social_cliente]
-      
-      --,[ct_cliente_consorcio]
-      --,[cc_lib_tributaria]
+      ,replace([ct_raz_social_cliente], char(13)+char(10),' ') 
       --,[cfl_servicio]
       , case when [cfl_servicio] = '1' then 'ESTUDIOS Y DISEÑOS'
 	  when [cfl_orig] = '2' then 'ESTUDIOS Y DISEÑOS'
 	  when [cfl_orig] = '3' then 'GERENCIAMIENTO DE PROYECTOS'
-	  else 'SIN TIPO DE SERVICIO'
+	  else 'OTROS SERVICIOS'
 	  end TipoServicio
-	  ,[cfl_orig]
-      --,[ct_desc_adicional] 
+	  ,[cfl_orig]   
 	   from dbo.PUB_TA_ANEXOS  
-	   --WHERE cc_area = 'TP' 
-	   UNION ALL 
-	   select 
-	      case when [cc_tipo] = '0' then 'CERTIFICADOS'
+	   WHERE coalesce(cc_area,'') <> '' and cc_area <> 'NN'
+UNION ALL 
+	  select  case when [cc_tipo] = '0' then 'CERTIFICADOS'
 	  when [cc_tipo] = '1' then 'CONTRATOS'
 	  when [cc_tipo] = '2' then 'ANEXOS'
 	  when [cc_tipo] = '4' then 'ADENDAS'
 	  else 'SIN DOCUMENTO'
 	  end TipoDoc,
-	    [cc_ano]
-      ,[cc_area]
-      ,[cc_costo]
-	  ,[ct_desc]
-	  ,[ct_moneda_orig]
-      ,[ni_importe_orig]
-	  ,[ni_importe_mc]
-	  ,[ni_tcambio]
-	  ,[df_inicio]
-      ,[df_termino]
-      ,[ct_ruta]
-           
+
+		[cc_ano] AñoProyecto
+      ,[cc_area] AreaProyecto
+      ,[cc_costo] CCostoProyecto
+	  --,replace([ct_desc], char(13)+char(10),' ') DescContractual
+	  ,replace((SELECT ct_desc_larga from ERP_TEST..PUB_TA_CCOSTOS where cc_cia = '01' and cc_costo = PUB_TA_ADENDAS.cc_costo), char(13)+char(10),' ') DescContractual
+	  ,[ct_moneda_orig] MonedaOrigen
+      ,[ni_importe_orig] ImporteMonedaOrigen
+	  ,[ni_importe_mc] ImporteMonedaComercialUS$
+	  ,[ni_tcambio] TCambio
+	  ,[df_inicio] FInicio
+      ,[df_termino] FTermino
+      ,replace([ct_ruta], char(13)+char(10),' ') RutaDocumento          
       ,[cc_ruc]
-      ,[ct_raz_social_cliente]
-      
-      --,[ct_cliente_consorcio]
-      --,[cc_lib_tributaria]
+      ,replace([ct_raz_social_cliente], char(13)+char(10),' ') 
       --,[cfl_servicio]
       , case when [cfl_servicio] = '1' then 'ESTUDIOS Y DISEÑOS'
 	  when [cfl_orig] = '2' then 'ESTUDIOS Y DISEÑOS'
 	  when [cfl_orig] = '3' then 'GERENCIAMIENTO DE PROYECTOS'
-	  else 'SIN TIPO DE SERVICIO'
+	  else 'OTROS SERVICIOS'
 	  end TipoServicio
-	  ,[cfl_orig]
-      --,[ct_desc_adicional] 
+	  ,[cfl_orig]   
 	   from dbo.PUB_TA_ADENDAS  
-	   --WHERE cc_area = 'TP'
-
+	   WHERE coalesce(cc_area,'') <> '' and cc_area <> 'NN'
 order by cc_ano desc, cc_costo desc, TipoDoc
 
 
